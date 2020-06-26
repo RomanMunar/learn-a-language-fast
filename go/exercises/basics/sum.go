5898e0392142d008f7741ff955234cc6b2f33346
romanmunar <roman.munar1@gmail.com>
2020-06-26 21:35:09 +0800

    'GO' added sum exercise in basics

---
+++ go/exercises/basics/sum.go
@@ -0,0 +1,11 @@
+package basics
+
+// Sum will sum up all of the numbers passed
+// in and return the result
+func Sum(numbers []int) int {
+	var sum int
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
+}
