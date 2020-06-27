5c1b2d59f5d8ab61f22d257907adcb803cf90e6c
romanmunar <roman.munar1@gmail.com>
2020-06-27 10:55:27 +0800

    'JS' main file

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+// Exercises in js have their own test files,
+// so just treat index.js as your scratchpad to test your solutions
+
+// All files exports their function by default
+// template for imports
+// import [FUNCTION_NAME] from "./exercises/[LEVEL]/[EXERCISE_FILE.js]"
+//
+
+import returnMe from "./exercises/basics/returnme.mjs"
+console.log(returnMe(1, 2))
