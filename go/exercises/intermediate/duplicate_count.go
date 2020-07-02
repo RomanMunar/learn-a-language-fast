4679209c3247291f98e28c0f61819dba32e32fc1
romanmunar <roman.munar1@gmail.com>
2020-07-02 08:17:59 +0800

    'GO & JS' added guide on duplicateCount

--- go/exercises/intermediate/duplicate_count.go
+++ go/exercises/intermediate/duplicate_count.go
@@ -1,3 +1,18 @@
+// Write a function that will
+// return the count of distinct case-insensitive alphabetic characters
+// that occur more than once in the input string.
+// The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Types
+// 	Arguments:	string
+// 	Return:			int
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+
 package intermediate
 
 import (
